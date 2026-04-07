ede0a5e50c746d0c3c420a0d35675bdc7f359bb7
lordyaiba12345 <lordyaiba12345@gmail.com>
2026-04-07 11:32:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       model: nimModel,
       messages: messages,
       temperature: temperature || 0.5,
-      max_tokens: max_tokens || 65536,
+      max_tokens: max_tokens || 32768,
       extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
       stream: stream || false
     };
